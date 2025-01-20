d26ef5d4ca0c7ba6e9cd12d6f9b2a6f898859764
Leon Gabriel <g73346920@gmail.com>
2025-01-20 13:36:50 +0300

    Chored:Each file created with a Base functional component && export default at the end of it.

---
+++ alx-movie-app/components/layouts/Header.tsx
@@ -0,0 +1,12 @@
+import { headers } from "next/headers";
+import React from "react";
+
+const Header:React.FC = () =>{
+  return(
+   <header>
+    Movie App
+   </header>
+  )
+}
+
+export default Header;
